1615ba84ecd7eda860069a640ef0cb5c5b809e59
hyunsss <mh81hj1@naver.com>
2024-02-17 04:24:48 +0900

    맵 그리드 구현
    추가적인 맵 피벗 기능 구현 예정

--- Assets/User/hyunwoo/Grid System/Scripts/Grid.cs
+++ Assets/User/hyunwoo/Grid System/Scripts/Grid.cs
@@ -7,27 +7,35 @@ using UnityEngine;
 using UnityEngine.UI;
 
 //그리드 클래스
-public class Grid : MonoBehaviour
-{
-    public GameObject gridObject;
-
+public class GridXY : MonoBehaviour
+{   //기본 Plane 오브젝트 스케일의 0.5배가 게임 내의 cell 1칸
     private int width;
     private int height;
     private float cellSize;
     private int[,] gridArray;
 
-    public Grid(int width, int height, float cellSize) {
+    public int Width => width;
+    public int Height => height;
+    public float CellSize => cellSize;
+    public int[,] GridArray => gridArray;
+
+    public void InitGrid(int width, int height, float cellSize)
+    {
         this.width = width;
         this.height = height;
         this.cellSize = cellSize;
 
         //각 cell 위치를 저장하는 배열
         gridArray = new int[width, height];
+    }
 
-
-        for(int x = 0; x < gridArray.GetLength(0); x++) {
-            for (int y = 0; y < gridArray.GetLength(1); y++) {
-                GenerateObject(GetWorldPosition(x, y), GetString(x, y));
+    public void GenerateGrid(GameObject cell)
+    {
+        for (int x = 0; x < gridArray.GetLength(0); x++)
+        {
+            for (int y = 0; y < gridArray.GetLength(1); y++)
+            {
+                DrawGrid(GetWorldPosition(x, y), GetString(x, y));
                 Debug.DrawLine(GetWorldPosition(x, y), GetWorldPosition(x, y + 1), Color.red, 100f);
                 Debug.DrawLine(GetWorldPosition(x, y), GetWorldPosition(x + 1, y), Color.red, 100f);
             }
@@ -42,40 +50,33 @@ public class Grid : MonoBehaviour
     /// <param name="x"></param>
     /// <param name="y"></param>
     /// <returns></returns>
-    private Vector3 GetWorldPosition(int x, int y) {
-        return new Vector3(x, 0, y) * cellSize;
+    public Vector3 GetWorldPosition(int x, int y)
+    {
+        return new Vector3(x, 0, y) * cellSize ;
     }
 
-    public string GetString(int x, int y) {
+    public string GetString(int x, int y)
+    {
         return $"{x}, {y}";
     }
 
-    private void GetXY(Vector3 worldPosition, out int x, out int y) {
+    public void GetXY(Vector3 worldPosition, out int x, out int y)
+    {
         x = Mathf.FloorToInt(worldPosition.x / cellSize);
         y = Mathf.FloorToInt(worldPosition.z / cellSize);
-
-        Debug.Log(x + "," + y);
     }
 
-    private void GenerateObject(Vector3 position, string Text) {
+    private void DrawGrid(Vector3 position, string Text)
+    {
         GameObject newgameObject = new GameObject($"{Text}");
-        newgameObject.transform.position = position + (new Vector3(cellSize, 0 ,cellSize) * 0.5f);
+        newgameObject.transform.position = position + (new Vector3(cellSize, 0, cellSize) * 0.5f);
         newgameObject.transform.Rotate(new Vector3(90, 0, 0));
         TextMesh textMesh = newgameObject.AddComponent<TextMesh>();
         textMesh.anchor = TextAnchor.MiddleCenter;
         textMesh.text = Text;
+
+        newgameObject.transform.parent = this.transform;
     }
 
-    /// <summary>
-    /// position을 그리드 좌표계로 변경 후 해당 셀 좌표에 있는 내용을 변경합니다.
-    /// </summary>
-    /// <param name="position">월드 좌표계</param>
-    /// <param name="Value">바꾸고자 하는 값</param>
-    public void SetValue(Vector3 position, int Value) {
-        int x, y;
-        GetXY(position, out x, out y);
 
-        GenerateObject(new Vector3(x, 0, y) * cellSize, Value.ToString());
-    }
-    
 }
---
+++ Assets/User/hyunwoo/TestBuilding.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestBuilding : MonoBehaviour
+{
+    private Transform anchor;
+
+    [SerializeField]private int areaWidth = 2;
+    [SerializeField]private int areaHeight = 2;
+
+    public int AreaWidth { get => areaWidth; }
+    public int AreaHeight { get => areaHeight; }
+    public Transform Anchor { get => anchor; }
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        anchor = transform.Find("Anchor");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
