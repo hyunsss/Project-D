51fb279033588a9af825bb62ed1f92d9b5e38b4d
FunnyP1ay <tlstjd042217@gmail.com>
2024-02-22 16:11:40 +0900

    Astar 완료, 시네머신, 인풋시스템 에셋 추갸ㅏ
    현재 경로 따라 이동 가능

---
+++ Assets/Prefabs/EnemyPrefabs/AStarTest/UnitAStar.cs
@@ -0,0 +1,51 @@
+using UnityEngine;
+using System.Collections;
+
+public class UnitAStar : MonoBehaviour
+{
+
+
+    public Transform target;
+    float speed = 20;
+    Vector3[] path;
+    int targetIndex;
+
+    void Start()
+    {
+        PathRequestManager.RequestPath(gameObject.transform.position, target.position, OnPathFound);
+        
+    }
+
+    public void OnPathFound(Vector3[] newPath, bool pathSuccessful)
+    {
+        if (pathSuccessful)
+        {
+            path = newPath;
+            targetIndex = 0;
+            StopCoroutine("FollowPath");
+            StartCoroutine("FollowPath");
+        }
+    }
+
+    IEnumerator FollowPath()
+    {
+        Vector3 currentWaypoint = path[0];
+        while (true)
+        {
+            if (transform.position == currentWaypoint)
+            {
+                targetIndex++;
+                if (targetIndex >= path.Length)
+                {
+                    yield break;
+                }
+                currentWaypoint = path[targetIndex];
+            }
+
+            transform.position = Vector3.MoveTowards(transform.position, currentWaypoint, speed * Time.deltaTime);
+            yield return null;
+
+        }
+    }
+
+}
