4d0aaf64be2c8391db4736e302f28128946c9eba
hyunsss <mh81hj1@naver.com>
2024-03-04 13:16:14 +0900

    업적 저장 불러오기 기능 구현 테스트 미완

--- Assets/User/hyunwoo/Script/VFXManager.cs
+++ Assets/User/hyunwoo/Script/VFXManager.cs
@@ -6,6 +6,16 @@ using UnityEngine.VFX;
 
 public class VFXManager : MonoBehaviour
 {
+
+    /*
+        Todo List : 
+        1. VFX 매니저 추가 수정 필요 ( 파티클이 실행되었을 때 방향, 크기 등 세분 조정 필요 )
+        2. 게임 저장 및 불러오기 기능 필요
+        3. 업적 클래스 다양하게 추가하고 모든 업적을 보여줄 수 있는 탭을 만들기. 업적이 달성 될 떄 마다 업데이트 해주는 팝업 창 만들기.
+    */
+
+
+
     public static VFXManager Instance;
     public Transform shotpoint;
     public enum VFXDir { Explosion }
