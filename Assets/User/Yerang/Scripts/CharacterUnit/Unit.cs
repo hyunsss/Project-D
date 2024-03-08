524ba5034894692c7bfb844bfc48813dd9fe2cce
JeonYerang <yooler0@naver.com>
2024-03-08 16:52:43 +0900

    GameDB에 추가
    .

--- Assets/User/Yerang/Scripts/CharacterUnit/Unit.cs
+++ Assets/User/Yerang/Scripts/CharacterUnit/Unit.cs
@@ -48,6 +48,7 @@ public class Unit : MonoBehaviour
 
     private void Die()
     {
+        GameDB.Instance.unit_Player.Remove(transform);
         Lean.Pool.LeanPool.Despawn(gameObject);
     }
 }
